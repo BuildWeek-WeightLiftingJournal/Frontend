1963e583df4311e3a5989d97f5c355901bf3a83d
Muamer Kukic <kukicako@gmail.com>
2019-10-22 14:13:19 -0700

    changes

--- src/Components/UpdateForm.js
+++ src/Components/UpdateForm.js
@@ -1,91 +1,100 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Field, withFormik } from "formik";
-import * as Yup from "yup";
 
-const WorkoutForm = ({ errors, touched, values, status }) => {
-  const [workout, setWorkout] = useState([]);
+
+const WorkoutForm = (props) => {
   
-  
-  useEffect(() => {
-    status && setWorkout(workout => [...workout, status]);
-  }, [status]);
+  const [workout, setworkout] = useState({
+    workoutName: '',
+    workoutDay: '', 
+    muscle: '',
+    exercises: [{
+    exercise: '',
+    weight: '',
+    sets: '',
+    reps: '',
+    }]
+  });
+
+  const update = (e) => {
+    e.preventDefault();
+     
+    }
+
+    const handleChange = e => {
+        setworkout({
+            ...workout,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
 
   return (
-    
-    <div className="update-form"> 
-          <h1>Update Workout</h1>
-      <Form>
-        <Field type="text" name="name" placeholder="workout Name" />
-        {touched.name && errors.name && (
-          <p className="error">{errors.name}</p>
-        )}
+    <>
+    <div>
+    <h1>Update</h1>
+      <form onSubmit={update}>
+        <input
+        type="text"
+        name="workout"
+        value={workout.workoutName}
+        placeholder="workout name"
+        onChange={handleChange}
+        />
+      
+      <input
+        type="text"
+        name="workoutDay"
+        value={workout.workoutDay}
+        placeholder="workout Day"
+        onChange={handleChange}
+        />
 
-        <Field type="text" name="date" placeholder="date" />
-        {touched.date && errors.date && <p className="error">{errors.date}</p>}
+        <input
+        type="text"
+        name="exercise"
+        value={workout.exercise}
+        placeholder="Exercise Name"
+        onChange={handleChange}
+        />
 
-        <Field type="text" name="exercise" placeholder="exercise name" />
-        {touched.exercise && errors.exercise && <p className="error">{errors.exercise}</p>}
-        
-        <Field type="text" name="weight" placeholder="weight" />
-        {touched.weight && errors.weight && <p className="error">{errors.weight}</p>}
+        <input
+        type="text"
+        name="weight"
+        value={workout.weight}
+        placeholder="weight"
+        onChange={handleChange}
+        />
 
-        <Field type="number" name="sets" placeholder="sets" />
-        {touched.sets && errors.sets && <p className="error">{errors.sets}</p>}
+        <input
+        type="text"
+        name="sets"
+        value={workout.sets}
+        placeholder="sets"
+        onChange={handleChange}
+        />
 
-        <Field type="number" name="reps" placeholder="reps" />
-        {touched.reps && errors.reps && <p className="error">{errors.reps}</p>}
+        <input
+        type="text"
+        name="reps"
+        value={workout.reps}
+        placeholder="reps"
+        onChange={handleChange}
+        />
 
-         <h2>Muscle Group</h2>
-        <Field component="select" className="group-select" name="group">
-          <option>Please Choose an Option</option>
-          <option value="UpperBody">Upper Body</option>
-          <option value="LowerBody">Lower Body</option>
-        </Field>
-        {touched.group && errors.group && <p className="error">{errors.group}</p>}
-        <button type="submit">Update!</button>
-      </Form>
- </div>
+
+        <select name="muscle">
+        <option value="upperBody">Upper Body</option>
+        <option value="lowerBody">lower Body</option>
+        </select>
+
+      
+        <button>Update!</button>
+      </form>
+      </div>
+    </>
   );
 };
 
-
-const FormikUserForm = withFormik({
-  mapPropsToValues({ name, date, group, exercise, reps, sets, weight }) {
-    return {
-      group: group || "",
-      name: name || "",
-      date: date || "",
-      exercise: exercise || "",
-      reps: reps || "",
-      sets: sets || "",
-      weight: weight || "",
-    };
-  },
-
-  validationSchema: Yup.object().shape({
-    name: Yup.string().required("Workout Name required"),
-    date: Yup.string().required("date Required"),
-    exercise: Yup.string().required("exercise Required"),
-    reps: Yup.string().required("reps Required"),
-    sets: Yup.string().required("sets Required"),
-    weight: Yup.string().required("weight Required"),
-    group: Yup.string()
-      .oneOf(["LowerBody", "UpperBody"])
-      .required("Please choose one!")
-  }),
-
-  handleSubmit(values, { setStatus }) {
-    axios
-      // values is our object with all our data on it.
-      .post("", values)
-      .then(res => {
-        setStatus(res.data);
-        console.log(res);
-      })
-      .catch(err => console.log(err.response));
-  }
-})(WorkoutForm); 
-console.log("This is the HOC", FormikUserForm);
-
-export default FormikUserForm;
+export default WorkoutForm;
