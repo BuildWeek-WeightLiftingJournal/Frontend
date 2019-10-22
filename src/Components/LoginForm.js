7d62e56b2fa24504693c05357ac40c2fd8b33cbf
Addison Hill <adddison.hill@gmail.com>
2019-10-22 12:23:35 -0500

    added axios post with dummy API

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,52 +1,66 @@
-import React from "react";
-// import { Form, Field, withFormik } from "formik";
-// import * as Yup from "yup";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
  
 
-const LoginForm = ({ errors, touched }) => {
-    // const [user, setUser] = useState([]);
+const LoginForm = ({ errors, touched, status }) => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        status && setUser([...user, status]);
+    }, [status]);
 
     return (
         <div className="container">
-            <form className="form">
+            <h2 className="title">Login</h2>
+            <Form className="form">
                 <label className="label">Username:
-                <input className="input"
+                <Field className="input"
                     type="text" 
                     name="username" 
                     placeholder="Username" />
                 </label>
-                {/* {touched.username && errors.username && (
+                {touched.username && errors.username && (
                     <p className="error">{errors.username}</p>
-                )} */}
+                )}
                 <label className="label">Password:
-                <input className="input"
+                <Field className="input"
                     type="text"
                     name="password"
                     placeholder="Password" />
                 </label>
-                {/* {touched.password && errors.password && (
+                {touched.password && errors.password && (
                     <p className="error">{errors.password}</p>
-                )} */}
+                )}
 
                 <button className="button" type="submit">Log In</button>
-            </form>
+            </Form>
         </div>
     );
 };
 
-// const FormikLoginForm = withFormik({
-//     mapPropsToValues({ username, password }) {
-//         return {
-//             username: username || "",
-//             password: password || ""
-//         };
-//     },
+const FormikLoginForm = withFormik({
+    mapPropsToValues({ username, password }) {
+        return {
+            username: username || "",
+            password: password || ""
+        };
+    },
 
-//     validationSchema: Yup.object().shape({
-//         username: Yup.string().required("Username required"),
-//         password: Yup.string().required("Password required")
-//     })
-// })(LoginForm);
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required("Username required"),
+        password: Yup.string().required("Password required")
+    }),
 
-export default LoginForm
+    handleSubmit(values, {setStatus}) {
+        axios
+        .post("https://reqres.in/api/users", values)
+        .then(res => {
+            setStatus(res.data);
+        })
+        .catch(error => console.log(error));
+    }
+})(LoginForm);
+
+export default FormikLoginForm
